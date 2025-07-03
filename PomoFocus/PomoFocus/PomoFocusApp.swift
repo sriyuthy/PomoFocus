a9ccebcfc328862078852cb9497edd7a8b518493
sriyuthy <sriyuth.y@gmail.com>
2025-07-02 22:01:46 -0500

    Add initial Xcode project for Pomodoro app

---
+++ PomoFocus/PomoFocus/PomoFocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomoFocusApp.swift
+//  PomoFocus
+//
+//  Created by Sriyuth Yerramshetty on 7/2/25.
+//
+
+import SwiftUI
+
+@main
+struct PomoFocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
