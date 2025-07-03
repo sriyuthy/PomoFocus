8db48f2b8e99db692fc0e88ee851d4f773c168dd
sriyuthy <sriyuth.y@gmail.com>
2025-07-02 22:22:27 -0500

    test run

--- PomoFocus/PomoFocus/PomoFocusApp.swift
+++ PomoFocus/PomoFocus/PomoFocusApp.swift
@@ -5,13 +5,31 @@
 //  Created by Sriyuth Yerramshetty on 7/2/25.
 //
 
+//test commit
+
 import SwiftUI
 
 @main
 struct PomoFocusApp: App {
+    let hello = "hey there, first time learning Swift!"
+    
+    func greet() {
+        
+        print(hello)
+
+    }
+    
+    init() {
+        
+        greet()
+        
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
+    
+            
 }
